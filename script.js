53ce0579774a5160e99952d5f6a88d817a5a8c82
Przemek Gałdys <galdys.przemek@gmail.com>
2018-02-01 10:05:20 +0100

    AWAX - further works

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.navbar a').click(function () {
+    $('a').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
             location.hostname == this.hostname) {
             var $target = $(this.hash);
@@ -18,32 +18,42 @@ $(document).ready(function () {
 });
 
 
-var d = document.getElementById("facts");
-var topPos = d.offsetTop;
+$(window).ready(function () {
+            $('.count1').countTo({
+                from: 0,
+                to: 1000,
+                speed: 3000,
+                refreshInterval: 90
+            });
+            $('.count2').countTo({
+                from: 0,
+                to: 900,
+                speed: 3000,
+                refreshInterval: 90
+            });
+            $('.count3').countTo({
+                from: 0,
+                to: 500,
+                speed: 3000,
+                refreshInterval: 90
+            });
+            $('.count4').countTo({
+                from: 0,
+                to: 100,
+                speed: 3000,
+                refreshInterval: 80
+            });
+})
 
-if (topPos > 500) {
-    $('.count1').countTo({
-        from: 0,
-        to: 1000,
-        speed: 3000,
-        refreshInterval: 90
-    });
-    $('.count2').countTo({
-        from: 0,
-        to: 900,
-        speed: 3000,
-        refreshInterval: 90
-    });
-    $('.count3').countTo({
-        from: 0,
-        to: 500,
-        speed: 3000,
-        refreshInterval: 90
-    });
-    $('.count4').countTo({
-        from: 0,
-        to: 100,
-        speed: 3000,
-        refreshInterval: 80
-    });
-}
+$(document).ready(function() {
+    let scrollY = 0;
+    $(window).scroll(function(){
+        scrollY = $(this).scrollTop();
+        if(scrollY >= 600) {
+            $('.scrollup').css('visibility', 'visible')
+        }
+        else if(scrollY < 600) {
+            $('.scrollup').css('visibility', 'hidden')
+        }
+    })
+});
