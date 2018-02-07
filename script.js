329bebe890974ecf96d3bb097a153c74261e78a2
Przemek Gałdys <galdys.przemek@gmail.com>
2018-02-07 16:48:12 +0100

    Next works

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(window).ready(function () {
 
-    $(document).scroll(function() {
+    $(document).scroll(function () {
         var scrollTop = $(window).scrollTop(),
             elementOffset = $('#facts').offset().top,
             distance = (elementOffset - scrollTop);
-            console.log(distance);
+        console.log(distance);
 
-        if (distance = 500) {
+        if (distance >= 500) {
             $('.count1').countTo({
                 from: 0,
                 to: 1000,
@@ -35,14 +35,14 @@ $(window).ready(function () {
     })
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     let scrollY = 0;
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         scrollY = $(this).scrollTop();
-        if(scrollY >= 600) {
+        if (scrollY >= 600) {
             $('.scrollup').css('visibility', 'visible')
         }
-        else if(scrollY < 600) {
+        else if (scrollY < 600) {
             $('.scrollup').css('visibility', 'hidden')
         }
     })
